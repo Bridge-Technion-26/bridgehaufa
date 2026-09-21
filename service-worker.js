b03a3bbe8ebabd1eb72200a3863f3c9b189ab041
ZVI GAL <zvigal5@gmail.com>
2026-09-21 12:52:46 +0300

    Update opening message

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "technion-bridge-v1";
-const SHELL = ["./", "./index.html", "./assets/style.css", "./manifest.webmanifest", "./assets/technion-icon.svg"];
+const CACHE_NAME = "technion-bridge-v2";
+const SHELL = ["./", "./index.html", "./assets/style.css", "./manifest.webmanifest", "./assets/technion-icon.svg", "./assets/technion-icon-192.png", "./assets/technion-icon-512.png"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
 self.addEventListener("fetch", event => {
